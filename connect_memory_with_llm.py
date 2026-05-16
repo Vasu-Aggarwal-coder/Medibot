ef85804688d08dfed40d4bad79c374bdd0d77502
Vasu-Aggarwal-coder <vasuaggarwal30102004@gmail.com>
2026-05-16 20:27:09 +0530

    changed connect memory because of streamlet share

--- connect_memory_with_llm.py
+++ connect_memory_with_llm.py
@@ -398,7 +398,10 @@ if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY not found!")
 
 GROQ_MODEL_NAME = "llama-3.1-8b-instant"
-DB_FAISS_PATH = "vectorstore/db_faiss"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_FAISS_PATH = os.path.join(BASE_DIR, "..", "vectorstore", "db_faiss")
+DB_FAISS_PATH = os.path.normpath(DB_FAISS_PATH)
 
 # -------------------- LLM --------------------
 print("Initializing LLM..........")
